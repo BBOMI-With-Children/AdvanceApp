a362c8f592c0cba2957afdc6460849548f0ffd4d
가현 <102145605+rkgus24@users.noreply.github.com>
2025-07-30 10:57:45 +0900

    add: 배너 추가, 스크롤 구현

--- AdvanceApp/BookList/BookListViewController.swift
+++ AdvanceApp/BookList/BookListViewController.swift
@@ -1,35 +1,82 @@
 //
-//  ViewController.swift
+//  BookListViewController.swift
 //  AdvanceApp
 //
 //  Created by 노가현 on 7/25/25.
 //
 
+import RxCocoa
+import RxSwift
+import SnapKit
+import Then
 import UIKit
 
 class BookListViewController: UIViewController {
-    private let banner = BannerView()
-
+    private let scrollView = UIScrollView().then {
+        $0.isPagingEnabled = true
+        $0.showsHorizontalScrollIndicator = false
+    }
+    
+    private let stackView = UIStackView().then {
+        $0.axis = .horizontal
+    }
+    
+    private let viewModel = BookListViewModel()
+    private let disposeBag = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         view.backgroundColor = .systemBackground
-
-        view.addSubview(banner)
-        banner.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            banner.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 0),
-            banner.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
-            banner.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20)
-        ])
-
-        let data = BannerViewData(
-            explain: "김초엽이 그리는\n디스토피아 속 희망",
-            title: "지구 끝의 온실",
-            imageName: "BookImage1",
-            backgroundColor: .systemPink.withAlphaComponent(0.2)
-        )
-        banner.configure(with: data)
+        setupLayout()
+        bindViewModel()
+        viewModel.loadBanner()
+    }
+    
+    private func setupLayout() {
+        view.addSubview(scrollView)
+        scrollView.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide).offset(20)
+            $0.leading.trailing.equalToSuperview()
+            $0.height.equalTo(160)
+        }
+        
+        scrollView.addSubview(stackView)
+        stackView.snp.makeConstraints {
+            $0.edges.equalTo(scrollView.contentLayoutGuide)
+            $0.height.equalTo(scrollView.frameLayoutGuide.snp.height)
+        }
+    }
+    
+    private func bindViewModel() {
+        viewModel.bannerData
+            .observe(on: MainScheduler.instance)
+            .bind(onNext: { [weak self] items in
+                guard let self = self else { return }
+                
+                self.view.layoutIfNeeded()
+                
+                self.stackView.arrangedSubviews.forEach { $0.removeFromSuperview() }
+                
+                for data in items {
+                    let pageContainer = UIView()
+                    self.stackView.addArrangedSubview(pageContainer)
+                    
+                    pageContainer.snp.makeConstraints { make in
+                        make.width.equalTo(self.scrollView.frameLayoutGuide.snp.width)
+                        make.height.equalTo(self.scrollView.frameLayoutGuide.snp.height)
+                    }
+                    
+                    let banner = BannerView()
+                    banner.configure(with: data)
+                    pageContainer.addSubview(banner)
+                    banner.snp.makeConstraints { make in
+                        make.center.equalToSuperview()
+                        make.width.equalTo(self.view.bounds.width - 40)
+                        make.height.equalToSuperview()
+                    }
+                }
+            })
+            .disposed(by: disposeBag)
     }
 }
 
--- AdvanceApp/Common/UIComponents/BannerView.swift
+++ AdvanceApp/Common/UIComponents/BannerView.swift
@@ -5,6 +5,8 @@
 //  Created by 노가현 on 7/29/25.
 //
 
+import SnapKit
+import Then
 import UIKit
 
 struct BannerViewData {
@@ -15,90 +17,68 @@ struct BannerViewData {
 }
 
 final class BannerView: UIView {
-    // MARK: - Subviews
+    private let containerView = UIView().then {
+        $0.layer.cornerRadius = 12
+    }
 
-    private let containerView: UIView = {
-        let v = UIView()
-        v.layer.cornerRadius = 12
-        return v
-    }()
+    private let explainLabel = UILabel().then {
+        $0.font = .systemFont(ofSize: 16, weight: .bold)
+        $0.textColor = .label
+        $0.numberOfLines = 2
+    }
 
-    private let explainLabel: UILabel = {
-        let lbl = UILabel()
-        lbl.font = .systemFont(ofSize: 16, weight: .bold)
-        lbl.textColor = .label
-        lbl.numberOfLines = 2
-        return lbl
-    }()
+    private let titleLabel = UILabel().then {
+        $0.font = .systemFont(ofSize: 12, weight: .medium)
+        $0.textColor = .secondaryLabel
+        $0.numberOfLines = 2
+    }
 
-    private let titleLabel: UILabel = {
-        let lbl = UILabel()
-        lbl.font = .systemFont(ofSize: 12, weight: .medium)
-        lbl.textColor = .secondaryLabel
-        lbl.numberOfLines = 1
-        return lbl
-    }()
+    private let imageView = UIImageView().then {
+        $0.contentMode = .scaleAspectFill
+        $0.clipsToBounds = true
+        $0.layer.cornerRadius = 4
+    }
 
-    private let imageView: UIImageView = {
-        let iv = UIImageView()
-        iv.contentMode = .scaleAspectFill
-        iv.clipsToBounds = true
-        iv.layer.cornerRadius = 4
-        return iv
-    }()
-
-    // MARK: - Initializers
+    // MARK: - Init
 
     override init(frame: CGRect) {
         super.init(frame: frame)
-        setupHierarchy()
-        setupConstraints()
+        setupUI()
     }
 
-    @available(*, unavailable)
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not supported")
-    }
+    @available(*, unavailable) required init?(coder: NSCoder) { fatalError() }
 
     // MARK: - Layout
 
-    override var intrinsicContentSize: CGSize {
-        return CGSize(width: UIView.noIntrinsicMetric, height: 160)
-    }
-
-    private func setupHierarchy() {
+    private func setupUI() {
         addSubview(containerView)
         [explainLabel, titleLabel, imageView].forEach { containerView.addSubview($0) }
+
+        containerView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+
+        explainLabel.snp.makeConstraints {
+            $0.leading.equalToSuperview().offset(25)
+            $0.top.equalToSuperview().offset(40)
+            $0.trailing.lessThanOrEqualTo(imageView.snp.leading).offset(-8)
+        }
+
+        titleLabel.snp.makeConstraints {
+            $0.leading.equalTo(explainLabel)
+            $0.top.equalTo(explainLabel.snp.bottom).offset(10)
+            $0.trailing.lessThanOrEqualTo(imageView.snp.leading).offset(-8)
+            $0.bottom.lessThanOrEqualToSuperview().offset(-8)
+        }
+
+        imageView.snp.makeConstraints {
+            $0.size.equalTo(CGSize(width: 72, height: 106))
+            $0.trailing.equalToSuperview().inset(36)
+            $0.centerY.equalToSuperview()
+        }
     }
 
-    private func setupConstraints() {
-        containerView.translatesAutoresizingMaskIntoConstraints = false
-        explainLabel.translatesAutoresizingMaskIntoConstraints = false
-        titleLabel.translatesAutoresizingMaskIntoConstraints = false
-        imageView.translatesAutoresizingMaskIntoConstraints = false
-
-        NSLayoutConstraint.activate([
-            containerView.leadingAnchor.constraint(equalTo: leadingAnchor),
-            containerView.trailingAnchor.constraint(equalTo: trailingAnchor),
-            containerView.topAnchor.constraint(equalTo: topAnchor),
-            containerView.bottomAnchor.constraint(equalTo: bottomAnchor),
-
-            explainLabel.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 25),
-            explainLabel.topAnchor.constraint(equalTo: containerView.topAnchor, constant: 40),
-            explainLabel.trailingAnchor.constraint(lessThanOrEqualTo: imageView.leadingAnchor, constant: -8),
-
-            titleLabel.leadingAnchor.constraint(equalTo: explainLabel.leadingAnchor),
-            titleLabel.topAnchor.constraint(equalTo: explainLabel.bottomAnchor, constant: 10),
-            titleLabel.bottomAnchor.constraint(lessThanOrEqualTo: containerView.bottomAnchor, constant: -8),
-
-            imageView.widthAnchor.constraint(equalToConstant: 72),
-            imageView.heightAnchor.constraint(equalToConstant: 106),
-            imageView.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -36),
-            imageView.centerYAnchor.constraint(equalTo: containerView.centerYAnchor)
-        ])
-    }
-
-    // MARK: - Configuration
+    // MARK: - Configure
 
     func configure(with data: BannerViewData) {
         containerView.backgroundColor = data.backgroundColor
